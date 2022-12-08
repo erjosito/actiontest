ddac208aab9c485b4430baf6af6aaba663bfa6ad
Jose Moreno <jose.moreno@microsoft.com>
2022-12-08 08:46:34 +0100

    added comments

--- scripts/base64encode.sh
+++ scripts/base64encode.sh
@@ -1,1 +1,7 @@
-cat $1 > $.base64
+#!/usr/bin/bash
+
+# Sample file to create a version of an existing file
+# Dumps the base64-encoded version of a file provided
+#   as parameter in a new file
+
+cat "$1" | base64 > "$1.base64"
